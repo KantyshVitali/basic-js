880fe95f07475467c299cf786525f2af4657647b
Виталий <kantyshvitali@gmail.com>
2020-09-23 18:19:39 +0300

    feat: realize carbon-dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+	let radioactiveDecay = 0.693 / HALF_LIFE_PERIOD;
+	
+	return (typeof(sampleActivity) === 'string' &&
+	 +sampleActivity <= 15 && +sampleActivity > 0)
+	 ? Math.ceil( Math.log(MODERN_ACTIVITY / +sampleActivity)
+	  / radioactiveDecay ) : false;
 };
