b1f7f921e44ae7c8689f7a120d4e5f39a23bf8f7
Виталий <kantyshvitali@gmail.com>
2020-09-26 00:54:01 +0300

    feat: add transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,57 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+	if (!Array.isArray(arr)) {
+		throw Error('');
+	}
+
+	let transformArray = arr.slice(0);
+
+	for (let i = 0; i < arr.length; i++) {
+		if (transformArray[i] === '--discard-next' ) {     
+      if(i == transformArray.length - 1) {
+        transformArray[i] = '--deleted--';
+      }else {
+        transformArray[i] = '--deleted--';
+        transformArray[i + 1] = '--deleted--';
+      }
+		} else 
+		if (transformArray[i] === '--discard-prev' ) {
+      if(i == 0) {
+        transformArray[i] = '--deleted--';
+      }else {
+        transformArray[i] = '--deleted--';
+        transformArray[i - 1] = '--deleted--';
+      }
+      
+		} else 
+		if (transformArray[i] === '--double-next' ) {
+       if(i == arr.length - 1) {
+         transformArray[i] = '--deleted--';
+      }else {
+        transformArray[i] = arr[i + 1];
+      }
+      
+		} else
+		if (transformArray[i] === '--double-prev' ) {
+      if(i == 0) {
+        transformArray[i] = '--deleted--';
+      }else {
+        transformArray[i] = transformArray[i - 1];
+      }
+        
+        
+		} else {
+      // transformArray[i] = arr[i];
+    }
+	}
+
+  for(let i = 0; i < transformArray.length; i++) {
+    if(transformArray[i] === '--deleted--') {
+      transformArray.splice(i, 1);
+      i--;
+    }
+  }
+
+	return transformArray;
 };
