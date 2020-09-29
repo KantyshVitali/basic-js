eaac96826275df49bf0865fb3af96789fa67bb31
Виталий <kantyshvitali@gmail.com>
2020-09-29 15:33:03 +0300

    feat: add extended-repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater( str, options = {} ) {
   
+  if(options.addition === null) {
+    options.addition = 'null';
+  }
+  if(options.separator === undefined) {
+    options.separator = '+';
+  }
+  if(options.additionSeparator === undefined) {
+    options.additionSeparator = '|';
+  }
+  if(options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+   if(options.additionRepeatTimes  === undefined) {
+    options.additionRepeatTimes = 1;
+  }
+  if(options.addition != undefined) {
+    let additionArray = [];
+    for(let i = 0; i < options.additionRepeatTimes; i++ ) {
+      additionArray.push(options.addition);
+    }
+    str += additionArray.join(options.additionSeparator);
+  } 
+  
+   
+	let resultArray = [];
+  for(let i = 0; i < options.repeatTimes; i++  ) {
+    resultArray.push(str);
+  }
+  return resultArray.join(options.separator);
+};
